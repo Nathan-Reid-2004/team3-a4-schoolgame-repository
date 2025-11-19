8f874c45caf645747032f7949617d47262c2a3f9
rowanvoigt <000970738@mohawkcollege.ca>
2025-11-18 19:12:19 -0500

    added movement and simple colour change logic

--- Game Movement Code/Game.cs
+++ Game Movement Code/Game.cs
@@ -1,5 +1,7 @@
 ﻿// Include the namespaces (code libraries) you need below.
+using Raylib_cs;
 using System;
+using System.ComponentModel;
 using System.Numerics;
 
 // The namespace your code is in.
@@ -11,14 +13,18 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-
+        float playerPosY = 100;
+        float playerPosX = 100;
+        bool isMoving = false;
+        Color playerColor = Color.Cyan;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetSize(1280, 800);
+            Window.SetTitle("Movement Shit");
         }
 
         /// <summary>
@@ -26,7 +32,64 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.White);
 
+            Movement();
+            PlayerColour();
+            if (isMoving == false)
+            {
+                playerColor=Color.Cyan;
+            }
+            else if (isMoving == true)
+            {
+                playerColor = Color.Yellow;
+            }
+            Draw.FillColor=(playerColor);
+            Draw.Circle(playerPosX, playerPosY, 40);
+
+            
+        }
+        public void Movement()
+        {
+            if (Input.IsKeyboardKeyDown(KeyboardInput.W))
+            {
+                playerPosY -= 6;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.S))
+            {
+                playerPosY += 6;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                playerPosX -= 6;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                playerPosX += 6;
+            }
+        }
+        public void PlayerColour()
+        {
+            if (Input.IsKeyboardKeyDown(KeyboardInput.W))
+            {
+                isMoving = true;
+            }
+            else if (Input.IsKeyboardKeyDown(KeyboardInput.S))
+            {
+                isMoving = true;
+            }
+            else if (Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                isMoving = true;
+            }
+            else if (Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                isMoving = true;
+            }
+            else
+            {
+                isMoving = false;
+            }
         }
     }
 
